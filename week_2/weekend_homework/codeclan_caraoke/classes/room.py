12e83996b9a6ef260907cd097b967269882318e7
Séamus <ryans40@tcd.ie>
2022-11-18 14:43:04 +0000

    completed basic functionality

--- week_2/weekend_homework/codeclan_caraoke/classes/room.py
+++ week_2/weekend_homework/codeclan_caraoke/classes/room.py
@@ -6,15 +6,26 @@ class Room:
         self.current_guests = []
         self.songs_available = []
     
-    def add_songs(self, song):
+    def add_song(self, song):
         self.songs_available.append(song)
 
-    def populate_room(self, guest):
-        self.current_guests.append(guest)
+    def add_playlist(self, songs):
+        self.songs_available.extend(songs)
+
+    def populate_room(self, guests):
+        if len(self.current_guests) + len(guests) <= self.capacity:
+            self.current_guests.extend(guests)
+        else:
+            return "Sorry, we're at capacity right now"
+
+    def add_individual(self, guest):
+        if len(self.current_guests) < self.capacity:
+            self.current_guests.append(guest)   
+        else:
+            return "Sorry, we're at capacity right now"  
 
     def empty_room(self):
         self.current_guests.clear()
 
     def remove_guest(self, guest):
         self.current_guests.remove(guest)
-
--- week_2/weekend_homework/codeclan_caraoke/tests/room_test.py
+++ week_2/weekend_homework/codeclan_caraoke/tests/room_test.py
@@ -9,10 +9,21 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room1 = Room("Room 1", 6)
         self.room2 = Room("Room 2", 8)
+        self.room3 = Room("Room 3", 3)
+        self.room4 = Room("Room 4", 4)
         self.guest1 = Guest("Barney")
         self.guest2 = Guest("Craig")
         self.guest3 = Guest("Steve")
-        self.guest_list = [(self.guest1), (self.guest2), (self.guest3)]
+        self.guest4 = Guest("Chris")
+        self.guest5 = Guest("Natasha")
+        self.guest6 = Guest("Mia")
+        self.guest_list = [self.guest1, self.guest2, self.guest3]
+        self.guest_list2 = [self.guest4, self.guest5, self.guest6]
+        self.song1 = Song("Crazy", "Patsy Cline")
+        self.song2 = Song("Song 2", "Blur")
+        self.song3 = Song("The Fear", "Lily Allen")
+        self.playlist = [self.song1, self.song2, self.song3]
+
 
     def test_room_has_room_id(self):
         self.assertEqual(self.room1.room_id, "Room 1")
@@ -26,14 +37,24 @@ class TestRoom(unittest.TestCase):
     def test_room_has_list_of_songs_available(self):
         self.assertEqual(self.room1.songs_available, [])
 
-    def test_can_add_songs_to_songs_available(self):
+    def test_can_add_song_to_songs_available(self):
         song1 = Song("Replay", "Iyaz")
-        self.room2.add_songs(song1)
+        self.room2.add_song(song1)
         self.assertEqual(self.room2.songs_available, [song1])
+
+    def test_can_add_playlist_to_songs_available(self):
+        input = len(self.playlist)
+        self.room1.add_playlist(self.playlist)
+        output = len(self.room1.songs_available)
+        self.assertEqual(input, output)
 
     def test_can_populate_room(self):
         self.room1.populate_room(self.guest_list)
-        self.assertEqual(self.room1.current_guests, [self.guest_list])
+        self.assertEqual(self.room1.current_guests, [self.guest1, self.guest2, self.guest3])
+
+    def test_can_add_individual_guest_to_room(self):
+        self.room1.add_individual(self.guest2)
+        self.assertEqual(self.room1.current_guests, [self.guest2])
 
     def test_can_empty_room(self):
         self.room1.populate_room(self.guest_list)
@@ -43,4 +64,30 @@ class TestRoom(unittest.TestCase):
     def test_check_out_individual_guest(self):
         self.room1.populate_room(self.guest_list)
         self.room1.remove_guest(self.guest2)
-        self.assertEqual(self.room1.current_guests, [(self.guest1), (self.guest3)])
+        self.assertEqual(self.room1.current_guests, [self.guest1, self.guest3])
+
+    def test_add_individual_when_room_at_capacity(self):
+        self.room3.populate_room(self.guest_list)
+        self.guest4 = Guest("Willie")
+        output = self.room3.add_individual(self.guest4)
+        self.assertEqual(output, "Sorry, we're at capacity right now")
+
+    def test_add_group_when_no_available_capacity(self):
+        self.room4.populate_room(self.guest_list)
+        output = self.room4.populate_room(self.guest_list2)
+        self.assertEqual(output, "Sorry, we're at capacity right now")
+
+    def test_add_group_when_available_capacity(self):
+        self.room2.populate_room(self.guest_list)
+        self.room2.populate_room(self.guest_list2)
+        self.assertEqual(self.room2.current_guests, 
+        [self.guest1, self.guest2, self.guest3, self.guest4, self.guest5, self.guest6])
+
+    
+        
+
+
+
+    
+
+    
