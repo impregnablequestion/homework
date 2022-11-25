ee318dc9054868f8979cb8488f931243b9e60f27
Séamus <ryans40@tcd.ie>
2022-11-25 18:03:07 +0000

    finished work for today, adding testing and styling tomorrow

---
+++ week_3/weekend_homework/smalltranslib/tests/book_test.py
@@ -0,0 +1,7 @@
+import unittest
+from models.book import Book
+
+class TestBook(unittest.TestCase):
+    def setUp(self):
+        self.book
+       
--- week_3/weekend_homework/smalltranslib/tests/run_tests.py
+++ week_3/weekend_homework/smalltranslib/tests/run_tests.py
@@ -1,3 +1,6 @@
 import unittest
 from tests.book_test import TestBook
 from tests.library_test import TestLibrary
+
+if __name__ == '__main__':
+    unittest.main()
