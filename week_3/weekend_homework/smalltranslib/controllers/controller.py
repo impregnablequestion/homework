3b454b910bb846754d219a9c08b549e03e226c81
Séamus <ryans40@tcd.ie>
2022-11-25 12:52:21 +0000

    added basic list of available books

--- week_3/weekend_homework/smalltranslib/controllers/controller.py
+++ week_3/weekend_homework/smalltranslib/controllers/controller.py
@@ -1,6 +1,14 @@
 from flask import render_template, redirect
 from app import app
+from models.library import book_list
+from models.book import Book
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/library/')
+def library():
+    return render_template("library.html", library = book_list)
+
+@app.route('/library/donate')
--- week_3/weekend_homework/smalltranslib/models/library.py
+++ week_3/weekend_homework/smalltranslib/models/library.py
@@ -8,4 +8,4 @@ book5 = Book("Stone Butch Blues", "Leslie Feinberg", "Memoir", False)
 book6 = Book("Detransition Baby", "Torrey Peters", "Fiction", False)
 book7 = Book("Confessions of the Fox", "Jordy Rosenberg", "Historical Fiction", True)
 
-library = [book1, book2, book3, book4, book5, book6, book7]
+book_list = [book1, book2, book3, book4, book5, book6, book7]
