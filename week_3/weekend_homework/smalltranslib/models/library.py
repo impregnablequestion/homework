07f3d457e73964efffc643758dc885d2e16a9cbb
Séamus <ryans40@tcd.ie>
2022-11-25 12:25:56 +0000

    added basic skeleton

---
+++ week_3/weekend_homework/smalltranslib/models/library.py
@@ -0,0 +1,11 @@
+from models.book import Book
+
+book1 = Book("Paul takes the form of a mortal girl", "Andrea Lawlor", "Speculative fiction", False)
+book2 = Book("People in Trouble", "Sarah Schulman", "Aids Memoir", False)
+book3 = Book("The Argonauts", "Maggie Nelson", "Auto-fiction", True)
+book4 = Book("Lote", "Shola von Reinhold", "Fiction", True)
+book5 = Book("Stone Butch Blues", "Leslie Feinberg", "Memoir", False)
+book6 = Book("Detransition Baby", "Torrey Peters", "Fiction", False)
+book7 = Book("Confessions of the Fox", "Jordy Rosenberg", "Historical Fiction", True)
+
+library = [book1, book2, book3, book4, book5, book6, book7]
