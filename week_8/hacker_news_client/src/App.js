0977cff13ec0d170a4b63f607573ee312850f6bd
Séamus <ryans40@tcd.ie>
2023-01-16 14:53:34 +0000

    added homework full stack

--- week_8/hacker_news_client/src/App.js
+++ week_8/hacker_news_client/src/App.js
@@ -2,10 +2,13 @@
 import './App.css';
 import Container from './containers/Container';
 
+const nonsense = "lalalala"
+
 function App() {
   return (
     <div className="App">
       <h1>hacker news</h1>
+      <h2>nonsense</h2>
       <Container/>
     </div>
   );
